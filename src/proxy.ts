0a3b5cb022b77d56aca8464322c99979ed938796
yamako-maxq <bbex31@gmail.com>
2026-04-17 19:04:05 +0900

    basic認証を解除

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,25 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import { verifyBasicAuthCredentials } from "./utils/auth/basicAuth";
 
 export const proxy = async (request: NextRequest) => {
-    const { nextUrl, headers } = request;
+    const { nextUrl} = request;
     const { pathname } = nextUrl;
 
-    // Basic認証
-    if (!verifyBasicAuthCredentials(
-        process.env.BASIC_AUTH,
-        headers.get("authorization"),
-        process.env.BASIC_AUTH_USERNAME,
-        process.env.BASIC_AUTH_PASSWORD
-    )) {
-        return NextResponse.json(
-            { error: "Basic Auth Required" },
-            {
-                headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
-                status: 401,
-            }
-        );
-    }
     // ルートパスにアクセスした場合、/searchにリダイレクトする
     if (pathname === "/" || pathname === "") {
         return NextResponse.redirect(new URL("/search/repositories", nextUrl))
